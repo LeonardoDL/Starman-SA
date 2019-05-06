ebe70501db3708d74a6e78f2ab5feb74f54db2d8
Leonardo <leo.del.lama@gmail.com>
2019-05-06 15:50:40 -0300

    Commit com o projeto

---
+++ StarmanCRUD/Controllers/ItemsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StarmanCRUD.Models;
+
+namespace StarmanCRUD.Controllers
+{
+    public class ItemsController : Controller
+    {
+        // GET: Items/Random
+        public ActionResult Random()
+        {
+            var item = new Item() { Name = "Pineapple" };
+            return View(item);
+        }
+    }
+}
